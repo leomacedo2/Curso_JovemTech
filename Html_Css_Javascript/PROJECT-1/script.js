a1b28e61bab2ebbed966df7aa45c4f58d6fd44a9
Leo Macedo <leomacedo19@gmail.com>
2026-06-03 21:22:21 -0300

    feat: Atualizações da segunda aula de html css e javasript

---
+++ Html_Css_Javascript/PROJECT-1/script.js
@@ -0,0 +1,7 @@
+function login(){
+    const email = document.getElementById("input-email").value
+    const senha = document.getElementById("input-senha").value
+
+    console.log(email, senha)
+
+}
